4fbbc95c06f70f82ae4a88be61beebd53992fe1b
Rikain <rikain4@gmail.com>
2022-03-20 00:02:06 +0100

    Added imperial units selection in menu and calculation, color to bmi value.

--- app/src/main/java/pl/edu/pwr/lab1/i242571/BMI.java
+++ app/src/main/java/pl/edu/pwr/lab1/i242571/BMI.java
@@ -1,7 +1,51 @@
 package pl.edu.pwr.lab1.i242571;
 
-public class BMI {
-    String CalculateBMI(decimal height, decimal mass){
 
+import android.content.Context;
+
+public class BMI {
+
+    private boolean metric = true;
+    private static final double metricToImperial = 0.0703;
+    private static final double underWeight = 18.5;
+    private static final double normalWeight = 24.9;
+    private static final double overWeight = 29.9;
+
+    void switchSystem(boolean to_metric){
+        metric = to_metric;
+    }
+
+    boolean validateInput(String height, String mass){
+        if (height == null || height.length() == 0 || mass == null || mass.length() == 0){
+            return false;
+        }
+        try {
+            Double.valueOf(height);
+            Double.valueOf(mass);
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    String calculateBMI(String height, String mass){
+        double bmi = Double.parseDouble(mass) / Math.pow(Double.parseDouble(height) / 100, 2);
+        if(!metric){
+            bmi = bmi * metricToImperial;
+        }
+        return String.format("%.2f", bmi);
+    }
+
+    int getBMIColor(Context context, String bmi){
+        double value = Double.parseDouble(bmi);
+        if (value < underWeight){
+            return context.getResources().getColor(R.color.blue);
+        }else if (value < normalWeight){
+            return context.getResources().getColor(R.color.green);
+        }else if (value < overWeight){
+            return context.getResources().getColor(R.color.orange);
+        }else{
+            return context.getResources().getColor(R.color.red);
+        }
     }
 }
--- app/src/main/java/pl/edu/pwr/lab1/i242571/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/lab1/i242571/MainActivity.java
@@ -1,19 +1,93 @@
 package pl.edu.pwr.lab1.i242571;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
 
+    private BMI bmi;
+    private TextView bmi_tv;
+    private EditText mass_et;
+    private EditText height_et;
+    private TextView mass_tv;
+    private TextView height_tv;
+    private Button count_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bmi = new BMI();
+        count_button = findViewById(R.id.button);
+        bmi_tv = findViewById(R.id.bmi_tv);
+        mass_et = findViewById(R.id.massInput);
+        height_et = findViewById(R.id.heightInput);
+        mass_tv = findViewById(R.id.mass_tv);
+        height_tv = findViewById(R.id.height_tv);
+
+        count_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String height = String.valueOf(height_et.getText());
+                String mass = String.valueOf(mass_et.getText());
+                if (bmi.validateInput(height, mass)){
+                    String bmi_value = bmi.calculateBMI(height, mass);
+                    bmi_tv.setText(bmi_value);
+                    bmi_tv.setTextColor(bmi.getBMIColor(MainActivity.this, bmi_value));
+                }else{
+                    //AlertDialog alert(MainActivity.this);
+                }
+                return;
+            }
+        });
+
         Log.d(TAG, "The onCreate() event");
+    }
+
+    protected void switchMeasurement(boolean isMetric){
+        if (isMetric){
+            mass_tv.setText(R.string.mass_tv_m);
+            height_tv.setText(R.string.height_tv_m);
+        }else{
+            mass_tv.setText(R.string.mass_tv_i);
+            height_tv.setText(R.string.height_tv_i);
+        }
+        bmi.switchSystem(isMetric);
+        return;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.measurement_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.metric_item:
+                switchMeasurement(true);
+                return true;
+            case R.id.imperial_item:
+                switchMeasurement(false);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
